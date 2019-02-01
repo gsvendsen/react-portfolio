5515ec850e42542f0b34001f470a451bac8a4670
Gustav Svendsen <gsvendsen99@gmail.com>
2019-02-01 10:15:18 +0100

    Updated skillset layout

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,37 +7,21 @@ class Skills extends React.Component {
       <div className="section">
           <div className="wrapper">
             <div className="skills-container">
+              <h3 className="skills-header">
+                Skills
+              </h3>
               <ul className="align-left">
-                <li>
-                  CSS
-                </li>
-                <li>
-                  JavaScript
-                </li>
-                <li>
-                  React.js
-                </li>
-                <li>
-                  Node
-                </li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>PHP</li>
+                <li>mySQL / SQLite</li>
               </ul>
-
-              <h2 className="skills-header">
-              Skills
-              </h2>
+              <h3 className="skills-header align-right">Learning</h3>
               <ul>
-                <li>
-                  PHP
-                </li>
-                <li>
-                  mySQL / SQLite
-                </li>
-                <li>
-                  Laravel
-                </li>
-                <li>
-                  Wordpress
-                </li>
+                <li>React.js</li>
+                <li>Node</li>
+                <li>Laravel</li>
+                <li>Wordpress</li>
               </ul>
               </div>
           </div>
