eb477ab9a1cdec769c9a3d8884404e0167270a3e
Gustav Svendsen <gsvendsen99@gmail.com>
2019-05-30 11:35:34 +0200

    Updates

--- precache-manifest.aa5f96280dc1cbdf2b8ffa4faa65cf64.js
+++ precache-manifest.aa5f96280dc1cbdf2b8ffa4faa65cf64.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/react-portfolio/static/js/main.1a216533.chunk.js"
   },
   {
-    "revision": "669a3bff009ea3034d85",
-    "url": "/react-portfolio/static/js/1.669a3bff.chunk.js"
+    "revision": "aebfbbdc7704e1044c8f",
+    "url": "/react-portfolio/static/js/1.aebfbbdc.chunk.js"
   },
   {
     "revision": "1a2165338a22d53b35d1",
     "url": "/react-portfolio/static/css/main.7f7bb845.chunk.css"
   },
   {
-    "revision": "563059825c82eb01c6a641c5c03f728a",
+    "revision": "8877c0e7053b7d64173d41b247bb654e",
     "url": "/react-portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.633e01313dd95a8e37a9111aa7f7b7e7.js"
+  "/react-portfolio/precache-manifest.aa5f96280dc1cbdf2b8ffa4faa65cf64.js"
 );
 
 workbox.clientsClaim();
